3fd99f969361ddf1bc3cb162bd1f8ccf74aa06c6
SaumyaaGarg-create <saumyaagarg30@gmail.com>
2025-08-25 23:53:01 +0530

    Initial commit from clean folder

--- ToneDeaf.ai/ToneDeaf.ai/options.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("saveKey").addEventListener("click", () => {
-    const key = document.getElementById("apiKey").value;
-    chrome.storage.sync.set({ openaiKey: key }, () => {
-        alert("API Key saved!");
-    });
-});
